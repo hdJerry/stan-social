fb6a9035b3330d12301dd982b0d974d13dd1e3fe
hdjerry <damilolajerryhd@gmail.com>
2020-03-20 13:42:57 +0100

    Stan Social commit

--- src/components/Contactus.vue
+++ src/components/Contactus.vue
@@ -4,17 +4,29 @@
 
     <div class="container">
 
+        <div class="caption-div">
+            <p class="caption">
+                One click away
+            </p>
+
+            <p class="sub-caption">
+                to become a Stan
+            </p>
+        </div>
+
     <div class="row">
         
         <div class="col-lg-2 col-md-2 col-sm-12"/>
         <div class="col-lg-4 col-md-4 col-sm-12">
             <div class="form-groupx">
                 <input type="text" placeholder="" class="form-field"/>
+                <i class="fa fa-user-o input-icon"></i>
             </div>
         </div>
         <div class="col-lg-4 col-md-4 col-sm-12">
             <div class="form-groupx">
                 <input type="text" placeholder="" class="form-field"/>
+                <i class="fa fa-user-o input-icon"></i>
             </div>
         </div>
 
@@ -23,25 +35,13 @@
         <div class="col-lg-4 col-md-4 col-sm-12">
             <div class="form-groupx">
                 <input type="text" placeholder="" class="form-field"/>
+                 <i class="fa fa-eye-slash input-icon"></i>
             </div>
         </div>
         <div class="col-lg-4 col-md-4 col-sm-12">
             <div class="form-groupx">
                 <input type="text" placeholder="" class="form-field"/>
-            </div>
-        </div>
-     
-
-     <div class="col-lg-2 col-md-2 col-sm-12"/>
-     <div class="col-lg-2 col-md-2 col-sm-12"/>
-        <div class="col-lg-4 col-md-4 col-sm-12">
-            <div class="form-groupx">
-                <input type="text" placeholder="" class="form-field"/>
-            </div>
-        </div>
-        <div class="col-lg-4 col-md-4 col-sm-12">
-            <div class="form-groupx">
-                <input type="text" placeholder="" class="form-field"/>
+                 <i class="fa fa-envelope-o input-icon"></i>
             </div>
         </div>
      
@@ -51,11 +51,13 @@
         <div class="col-lg-4 col-md-4 col-sm-12">
             <div class="form-groupx">
                 <input type="text" placeholder="" class="form-field"/>
+                <i class="fa fa-mobile input-icon"></i>
             </div>
         </div>
         <div class="col-lg-4 col-md-4 col-sm-12">
             <div class="form-groupx">
                 <input type="text" placeholder="" class="form-field"/>
+                <i class="fa fa-map-marker input-icon"></i>
             </div>
         </div>
      
@@ -65,17 +67,75 @@
         <div class="col-lg-4 col-md-4 col-sm-12">
             <div class="form-groupx">
                 <input type="text" placeholder="" class="form-field"/>
+                 <i class="fa fa-flag-o input-icon"></i>
             </div>
         </div>
         <div class="col-lg-4 col-md-4 col-sm-12">
             <div class="form-groupx">
                 <input type="text" placeholder="" class="form-field"/>
+                 <i class="fa fa-suitcase input-icon"></i>
+            </div>
+        </div>
+     
+
+     <div class="col-lg-2 col-md-2 col-sm-12"/>
+     <div class="col-lg-2 col-md-2 col-sm-12"/>
+        <div class="col-lg-4 col-md-4 col-sm-12">
+            <div class="form-groupx">
+                <input type="text" placeholder="" class="form-field"/>
+                <i class="fa fa-exclamation-circle input-icon"></i>
+            </div>
+        </div>
+        <div class="col-lg-4 col-md-4 col-sm-12">
+            <div class="form-groupx socials-form">
+                <input type="text" placeholder="" class="form-field"/>
+                <input type="text" placeholder="" class="form-field input-border-left"/>
+                <i class="fa fa-instagram input-icon"></i>
+            </div>
+        </div>
+
+
+         <div class="col-lg-2 col-md-2 col-sm-12"/>
+     <div class="col-lg-2 col-md-2 col-sm-12"/>
+        <div class="col-lg-4 col-md-4 col-sm-12">
+           <div class="form-groupx socials-form">
+                <input type="text" placeholder="" class="form-field"/>
+                <input type="text" placeholder="" class="form-field input-border-left"/>
+                <i class="fa fa-facebook input-icon"></i>
+            </div>
+        </div>
+        <div class="col-lg-4 col-md-4 col-sm-12">
+            <div class="form-groupx socials-form">
+                <input type="text" placeholder="" class="form-field"/>
+                <input type="text" placeholder="" class="form-field input-border-left"/>
+                <i class="fa fa-youtube-play input-icon"></i>
+            </div>
+        </div>
+
+            <div class="col-lg-2 col-md-2 col-sm-12"/>
+     <div class="col-lg-2 col-md-2 col-sm-12"/>
+        <div class="col-lg-4 col-md-4 col-sm-12">
+            <div class="form-groupx socials-form">
+                <input type="text" placeholder="" class="form-field"/>
+                <input type="text" placeholder="" class="form-field input-border-left"/>
+                <i class="fa fa-twitter input-icon"></i>
+            </div>
+        </div>
+        <div class="col-lg-4 col-md-4 col-sm-12">
+            <div class="form-groupx socials-form">
+                <input type="text" placeholder="" class="form-field"/>
+                <input type="text" placeholder="" class="form-field input-border-left"/>
+                <i class="fa fa-linkedin input-icon"></i>
             </div>
         </div>
      
      
 
     </div>
+
+    <button type="submit" class="btn-stan">Become a Stan</button>
+
+    
 
     </div>
 
@@ -89,25 +149,75 @@
 #contactus{
     margin-top: 100px;
     color: #fff;
-    background-color: #C90404;
+    background-color: #760000;
     background-attachment: fixed;
     padding-top: 20px;
     margin-bottom: 40px;
+    padding-bottom: 40px;
+}
+
+.caption-div{
+    margin-top: -10px;
+    margin-bottom: 40px;
+    .caption{
+        font-size: 2.8rem;
+        font-weight: 700;
+    }
+
+    .sub-caption{
+        font-size: 1.3rem;
+        margin-top: -30px;
+    }
 }
 
 .form-groupx{
     border: 1px solid #fff;
     margin-bottom: 20px;
     border-radius: 20px;
+    padding: 10px;
 
     input{
         border: none;
-        padding: 7px;
+        // padding: 7px;
         outline: none;
         background-color: transparent;
         color: #fff;
         font-size: 1rem;
     }
+
+    .input-icon{
+        position: relative;
+        font-size: 1.0rem;
+        color: #fff;
+        top : 2px;
+        right: -25px;
+    }
+}
+
+.socials-form{
+   
+   input{
+       width: 120px;
+   }
+
+   .input-border-left{
+       border-left: 1px solid #fff;
+    padding-left: 10px;
+
+   }
+}
+
+
+.btn-stan{
+    display: block;
+    width: 300px;
+    font-size: 1.2rem;
+    padding: 20px;
+    color:#760000;
+    background-color: #fff;
+    border-radius: 30px;
+    border: none;
+    margin: 20px auto;
 }
 
 </style>
